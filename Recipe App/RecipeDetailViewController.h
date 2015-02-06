58b7d7903ae17a120b9de2ea07e8cca80b478ebb
gguerrero11 <gguerrero11@gmail.com>
2015-02-06 15:46:33 -0700

    stuff

--- Recipe App/RecipeDetailViewController.h
+++ Recipe App/RecipeDetailViewController.h
@@ -12,4 +12,6 @@
 
 @property (nonatomic,assign) NSInteger indexRecipes;
 
+- (float) sizeOfPreviousView:(UIView *)nameOfView;
+
 @end
--- Recipe App/RecipeDetailViewController.m
+++ Recipe App/RecipeDetailViewController.m
@@ -13,16 +13,18 @@
 @interface RecipeDetailViewController ()
 
 @property (nonatomic, strong) UIScrollView *detailScrollView;
-//@property (nonatomic, strong) NSInteger ingredienteKey;
-
 
 @end
 
 @implementation RecipeDetailViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    float newViewYOrigin = 0;
+    float paddingBetweenViews = 30;
     
     //create the scrollView for Description and Ingredientes
     self.view.backgroundColor = [UIColor whiteColor];
@@ -30,37 +32,41 @@
     [self.view addSubview: self.detailScrollView];
     
     //creating a label for the description of the recipe
+    UILabel *descrip = [[UILabel alloc]initWithFrame:CGRectMake(20,0, (self.view.frame.size.width - 30), 0)];
     NSString *recipeDescription = [RARecipes descriptionAtIndex:self.indexRecipes];
-    UILabel *descrip = [[UILabel alloc]initWithFrame:CGRectMake(20,0, (self.view.frame.size.width - 30), 150)];
     descrip.numberOfLines = 0;
     descrip.text = recipeDescription;
     [descrip sizeToFit];
     [self.detailScrollView addSubview:descrip];
     
     // Creating a separate view window to be below description
-    float padding = 30;
-    float sizeOfDescriptionView = descrip.frame.size.height + descrip.frame.origin.y + padding;
-    UIView *ingredientsWithVolume = [[UIView alloc]initWithFrame:CGRectMake(20, sizeOfDescriptionView,self.view.frame.size.width,self.view.frame.size.height)];
+    newViewYOrigin += descrip.frame.size.height + paddingBetweenViews;
+    NSLog(@"%f", newViewYOrigin);
+    
+    UIView *ingredientsWithVolume = [[UIView alloc]initWithFrame:CGRectMake(20, newViewYOrigin, self.view.frame.size.width, self.view.frame.size.height)];
     [self.detailScrollView addSubview:ingredientsWithVolume];
     
-    //Creating label for the ingredientes of the recipe
-    for (int i = 0; i < [RARecipes ingredientCountAtIndex:self.indexRecipes]; i++) {
-        UILabel *ingredientsKey = [[UILabel alloc]initWithFrame:CGRectMake(0, i*30, self.view.frame.size.width, 30)];
-        ingredientsKey.text = [RARecipes ingredientTypeAtIndex:i inRecipeAtIndex:self.indexRecipes];
-        [ingredientsKey sizeToFit];
-        [ingredientsWithVolume addSubview:ingredientsKey];
-        
-        //Creating label for the ingrediente's volume of the recipe
-        UILabel *volumeKey = [[UILabel alloc]initWithFrame:CGRectMake(200, i*30, self.view.frame.size.width, 30)];
-        volumeKey.numberOfLines = 0;
-        volumeKey.text = [RARecipes ingredientVolumeAtIndex:i inRecipeAtIndex:self.indexRecipes];
-        [volumeKey sizeToFit];
-        [ingredientsWithVolume addSubview:volumeKey];
-    }
-
     
+        //Creating label for the ingredientes of the recipe
+        for (int i = 0; i < [RARecipes ingredientCountAtIndex:self.indexRecipes]; i++) {
+            UILabel *ingredientsKey = [[UILabel alloc]initWithFrame:CGRectMake(0, i*30, self.view.frame.size.width, 30)];
+            ingredientsKey.text = [RARecipes ingredientTypeAtIndex:i inRecipeAtIndex:self.indexRecipes];
+            [ingredientsKey sizeToFit];
+            [ingredientsWithVolume addSubview:ingredientsKey];
+                NSLog(@"Pass: %i: %f", i, newViewYOrigin);
+            
+            //Creating label for the ingrediente's volume of the recipe
+            UILabel *volumeKey = [[UILabel alloc]initWithFrame:CGRectMake(200, i*30, self.view.frame.size.width, 30)];
+            volumeKey.numberOfLines = 0;
+            volumeKey.text = [RARecipes ingredientVolumeAtIndex:i inRecipeAtIndex:self.indexRecipes];
+            [volumeKey sizeToFit];
+            [ingredientsWithVolume addSubview:volumeKey];
+        }
+    NSLog(@"Volume key: %f", ingredientsWithVolume.frame.size.height);
+    newViewYOrigin += ingredientsWithVolume.frame.size.height + paddingBetweenViews;
+    //NSLog(@"%f", newViewYOrigin);
     //create view view for the recipe's directions
-    UIView *recipeDirections = [[UIView alloc]initWithFrame:CGRectMake(20, 400, self.view.frame.size.width, self.view.frame.size.height)];
+    UIView *recipeDirections = [[UIView alloc]initWithFrame:CGRectMake(20, newViewYOrigin, self.view.frame.size.width, self.view.frame.size.height)];
     [self.detailScrollView addSubview:recipeDirections];
 
 
@@ -87,7 +93,6 @@
         
     }
     
-
     // Combined height value of all views
     float contentHeight = recipeDirections.frame.size.height + ingredientsWithVolume.frame.size.height + descrip.frame.size.height;
     self.detailScrollView.contentSize = CGSizeMake(self.view.frame.size.width, contentHeight);
@@ -95,6 +100,11 @@
     
 }
 
+// Automatically calculates size of the View and how to place it under the previous view.
+- (float) sizeOfPreviousView:(UIView *)nameOfView {
+    float sizeofPreviousViewFloat = nameOfView.frame.size.height + nameOfView.frame.origin.y;
+    return sizeofPreviousViewFloat;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
